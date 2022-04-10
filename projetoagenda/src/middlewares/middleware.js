a12f11ca4b92e463fca9351edf1e04ccda08cefd
Samuel Monteiro <samuelsilva91935@gmail.com>
2022-04-10 18:53:08 -0300

    Correção do padrão MVC

--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -1,21 +1,19 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local'
-    next();
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  next();
 };
 
 exports.outroMiddleware = (req, res, next) => {
-    next();
+  next();
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err) {
-        return res.render('404');
-    }
-    
-    next();
+  if(err && 'EBADCSRFTOKEN' === err.code) {
+    return res.render('404');
+  }
 };
 
 exports.csrfMiddleware = (req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.locals.csrfToken = req.csrfToken();
+  next();
 };
