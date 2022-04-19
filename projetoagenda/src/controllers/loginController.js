dc982e808c4f2d93558fbd9a0b572ad1a77f9bdd
Samuel Monteiro <samuelsilva91935@gmail.com>
2022-04-19 11:02:19 -0300

    Logando o usuário

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,7 +1,9 @@
+const session = require('express-session');
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -27,3 +29,33 @@ exports.register = async function(req, res) {
         return res.render('404');
     }
 };
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors); // mostra o erro
+            req.session.save(function() {
+                return res.redirect('/login/index');  //nos mandará para a pagina que já estavamos caso ocorrer um erro
+            });
+            return; //retorna o if
+        }
+    
+        req.flash('success', 'Você entrou no sistema'); // entrou
+        req.session.user = login.user;
+        req.session.save(function() {
+            return res.redirect('/login/index');  //nos mandará para a pagina que já estavamos informando successo
+        });
+
+    }  catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
